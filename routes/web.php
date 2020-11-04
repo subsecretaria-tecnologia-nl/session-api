04a03490b376d6e824e900da88aa58cdf95e21a8
Raymundo Salazar <raymundo.salser@hotmail.com>
2020-11-04 15:06:35 -0600

    options

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 $router->options("*", function(){
 	return true;
-})
+});
+
 $router->group(["middleware" => "json.schema.validation", "prefix" => (getenv("APP_PREFIX") ?? "")], function () use ($router) {
 	$router->get('/', function() {
 		return "THIS ROUTE DOES NOT EXISTS.";
