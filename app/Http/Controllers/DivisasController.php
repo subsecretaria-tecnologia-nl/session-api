c6a13fe9534eb5f1e1471f8368a40cc483b394a1
karlaces <karla.ces@outlook.com>
2020-12-23 16:57:51 -0600

    get cambio divisas modificacion

--- app/Http/Controllers/DivisasController.php
+++ app/Http/Controllers/DivisasController.php
@@ -114,10 +114,18 @@ class DivisasController extends Controller
 
     public function getCambioDivisa(Request $request){
         $parametro =$request->parametro;
+        $monto =$request->monto;
         $token= "b001d846c931ffbc16701040b9f669e28c00e5e36714f4efcbeaee386de4985c";
         try {
             $url="https://www.banxico.org.mx/SieAPIRest/service/v1/series/$parametro/datos/oportuno?token=$token";
             $data = json_decode(file_get_contents($url), true);
+
+            $costo = $data["bmx"]["series"][0]["datos"][0]["dato"];
+            $resultado = floatval($costo) * $monto;
+            $valor = bcdiv($resultado, '1', 4);
+
+            $data["resultado"]=array("precio_final"=>$valor, "costo_divisa"=>$costo, "monto"=>$monto);
+
             return $data;
    
         } catch (\Exception $e) {
